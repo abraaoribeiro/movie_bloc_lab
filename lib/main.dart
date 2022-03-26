69de360af00b3bdcb5c1563e25bdf3e9a859f534
abraaoribeiro <abraaohdg@gmail.com>
2022-03-25 23:02:06 -0300

    feat: BlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_bloc_lab/app.dart';
+import 'package:movie_bloc_lab/app_oberver.dart';
 import 'package:movie_bloc_lab/config/dependency_injection.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  runApp(const MovieApp());
+  BlocOverrides.runZoned(() => runApp(const MovieApp()),
+    blocObserver: MovieAppObserver());
 }
